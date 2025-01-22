0515980662b027bc9558dead8bb5880ec5e68c14
Deepanshu Choudhary <deepanshuchoudhary162002@gmail.com>
2025-01-23 03:28:59 +0530

    Connect MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/db.js";
 
 dotenv.config({
     path:'./.env'
-
+    
 })
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log(`Server is running on host: ${process.env.PORT}`);
-})
-app.on("error",(error)=>{
-    console.log("Error:", error)
-    throw error;
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`Server is running on host: ${process.env.PORT}`);
+    })
+    app.on("error",(error)=>{
+        console.log("Error:", error)
+        throw error;
+    })
 })
