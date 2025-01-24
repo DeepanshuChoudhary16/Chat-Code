36559aa2fee30553ca6e0c77b0e9c062cacb88b6
Deepanshu Choudhary <deepanshuchoudhary162002@gmail.com>
2025-01-25 04:03:47 +0530

    Add  Log out route

--- backend/routers/user.routes.js
+++ backend/routers/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {registerUser,loggedinUser, profileController} from '../controllers/user.controller.js'
+import {registerUser,loggedinUser, profileController ,loggedoutUser} from '../controllers/user.controller.js'
 import { authUser } from '../middleware/auth.middleware.js'
 
 const router = Router()
@@ -7,4 +7,5 @@ const router = Router()
 router.route("/register").post(registerUser)
 router.route('/login').post(loggedinUser)
 router.route('/profile').get(authUser,profileController)
+router.route('/logout').get(authUser,loggedoutUser)
 export default router;
