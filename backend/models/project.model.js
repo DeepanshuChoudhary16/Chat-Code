36a14dfb40cd2d8d144ef15c46d8d1b0d0ad6a63
Deepanshu Choudhary <deepanshuchoudhary162002@gmail.com>
2025-01-26 20:44:00 +0530

    Add project model

---
+++ backend/models/project.model.js
@@ -0,0 +1,19 @@
+import mongoose,{Schema} from 'mongoose'
+
+const projectSchema = new Schema({
+    name:{
+        type:String,
+        lowercase: true,
+        required: true,
+        trim: true,
+        unique: [true,"project must be unique"]
+    },
+    users:[
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }]
+})
+
+const projectModel = mongoose.model('Project',projectSchema)
+export default projectModel;
