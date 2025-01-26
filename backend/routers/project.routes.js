b0100303d238e291468a84d17b1c9b9f8a3a2215
Deepanshu Choudhary <deepanshuchoudhary162002@gmail.com>
2025-01-27 03:07:58 +0530

    Add All project route

--- backend/routers/project.routes.js
+++ backend/routers/project.routes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
-import { createProject } from '../controllers/project.controller.js';
+import { createProject,getAllProject } from '../controllers/project.controller.js';
 import { authUser } from '../middleware/auth.middleware.js';
 const router = Router();
 
 router.route("/create").post(authUser,createProject)
-
+router.route("/all").get(authUser,getAllProject)
+router.route("/add-user").put(authUser,addUserToProject)
 export default router
