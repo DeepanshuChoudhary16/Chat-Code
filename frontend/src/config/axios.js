6ffd8cda4e5e2c00804e2095e139e6bbd77a607b
Deepanshu Choudhary <deepanshuchoudhary162002@gmail.com>
2025-01-27 03:10:23 +0530

    Add header for token

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URI
+    baseURL: import.meta.env.VITE_API_URI,
+    headers:{
+        "Authorization":`Bearer ${localStorage.getItem('token')}`
+    }
 })
 
 export default axiosInstance
