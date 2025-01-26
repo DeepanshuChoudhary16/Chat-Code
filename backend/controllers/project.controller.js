ec13e8abf08f234fc5aeb741fba505fd4398f925
Deepanshu Choudhary <deepanshuchoudhary162002@gmail.com>
2025-01-26 20:43:00 +0530

    Add project endpoint

---
+++ backend/controllers/project.controller.js
@@ -0,0 +1,39 @@
+import projectModel from "../models/project.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import User from "../models/user.model.js";
+
+const createProject = asyncHandler(async(req,res)=>{
+   try {
+        const {name} = req.body;
+
+        const loggedinUser = await User.findOne({email: req.user.email});
+        const userId = loggedinUser._id;
+     
+        if(!name || !userId)
+        {
+            throw new ApiError(401,"Name or User is required");
+        }
+        const newProject = await projectModel.create({
+            name,
+            users:[userId]
+        })
+        console.log("creation of project is compelet");
+        return res
+        .status(201)
+        .json(
+            new ApiResponse(
+                200,
+                {newProject},
+                "Project created successfully"
+            )
+        )
+
+    } catch (error) {
+        console.log(error)
+        throw new ApiError(400,"Project not created successfully");
+    }
+})
+
+export {createProject};
